2b5d36eaf5011b670998277eafe91adad15aad83
Colin Ho <colinho97@gmail.com>
2023-09-26 00:43:09 -0400

    added in basic semantics for Oauth spotify

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,124 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  // Code Verifier
+  function generateRandomString(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+  
+  // Code Challenge
+
+  async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+    }
+  
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+  
+    return base64encode(digest);
+  }
+  
+  // Request User Authorization
+
+  const clientId = '712443b6878f405580d4e8323b9980d1';
+const redirectUri = 'https://adorable-kitsune-619f1d.netlify.app/';
+
+let codeVerifier = generateRandomString(128);
+
+generateCodeChallenge(codeVerifier).then(codeChallenge => {
+  let state = generateRandomString(16);
+  let scope = 'user-read-private user-read-email';
+
+  localStorage.setItem('code_verifier', codeVerifier);
+
+  let args = new URLSearchParams({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scope,
+    redirect_uri: redirectUri,
+    state: state,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge
+  });
+
+  window.location = 'https://accounts.spotify.com/authorize?' + args;
+});
+
+  const urlParams = new URLSearchParams(window.location.search);
+  let code = urlParams.get('code');
+
+  // Request an access token
+
+  let codeVerifierToken = localStorage.getItem('code_verifier');
+
+  let body = new URLSearchParams({
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: redirectUri,
+    client_id: clientId,
+    code_verifier: codeVerifierToken
+  });
+
+
+  const response = fetch('https://accounts.spotify.com/api/token', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: body
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('HTTP status ' + response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    localStorage.setItem('access_token', data.access_token);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+  async function getProfile(accessToken) {
+    let accessToken = localStorage.getItem('access_token');
+  
+    const response = await fetch('https://api.spotify.com/v1/me', {
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      }
+    });
+  
+    const data = await response.json();
+    setData(data)
+  }
+
+
+    const [data, setData] = useState({});
+
+
+
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
